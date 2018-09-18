259ed27c6b18882b73cebf60f69b363f678d57cd
muschellij2 <muschellij2@gmail.com>
2018-09-18 19:14:54 -0400

    Added the requirements.

--- app.R
+++ app.R
@@ -1,48 +1,138 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+library(transcriptr)
+library(dplyr)
+
+scm = read.csv("scm.csv", header = TRUE, as.is = TRUE)
+scm = scm %>% 
+    filter(!course_number %in% "")
+scm$year = scm$term = NULL
+scm$program = "SCM: Biostatistics"
+
+phd = read.csv("phd.csv", header = TRUE, as.is = TRUE)
+phd = phd %>% 
+    filter(!course_number %in% "")
+phd$year = phd$term = NULL
+phd$program = "PHD: Biostatistics"
+
+curr = rbind(scm, phd)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
+    titlePanel("Parse Transcript"),
+    
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            fileInput("pdf", "Choose Transcript File(s)",
+                      accept = c("application/pdf",
+                                 ".pdf"),
+                      multiple = TRUE
+            ),
+            checkboxInput("grade", "Include Grades?", 
+                          value = TRUE),
+            downloadButton("downloadData", "Download")
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            # Output: Tabset w/ plot, summary, and table ----
+            tabsetPanel(
+                type = "tabs",
+                tabPanel("Transcript", dataTableOutput("tab")),
+                tabPanel("Report", dataTableOutput("report"))
+            )            
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    
+    get_df = function() {
+        req(input$pdf)
+        
+        # when reading pdf, may error
+        tryCatch(
+            {
+                print(input$pdf)
+                # print(dput(input$pdf))
+                paths = input$pdf$datapath
+                df <- lapply(
+                    paths, 
+                    read_transcript,
+                    type = "jhu")
+                df = mapply(function(x, name) {
+                    x$file = name
+                    id = attributes(x)$student_info["student_id"]
+                    x$student_id = id
+                    x
+                }, df, input$pdf$name, SIMPLIFY = FALSE)
+                
+                df = do.call(rbind, df)
+                if (!input$grade) {
+                    df$grade = NULL
+                }
+            },
+            error = function(e) {
+                # return a safeError if a parsing error occurs
+                stop(safeError(e))
+            }
+        )
+        return(df)  
+    }
+    output$tab <- renderDataTable({
+        get_df()
+    }, options = list(pageLength = 10))
+    
+    
+    # Downloadable csv of selected dataset ----
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            "output.csv"
+        },
+        content = function(file) {
+            write.csv(get_df(), file, row.names = FALSE)
+        }
+    )   
+    
+    output$report <- renderDataTable({
+        df = get_df()
+        uprog = unique(df$program)
+        xcurr = curr %>% 
+            filter(program %in% uprog)
+        if (nrow(xcurr) == 0) {
+            return(NULL)
+        }
+        df = full_join(xcurr, df, 
+                       by = c("course_number", "program"))
+        df = df %>% 
+            rename(course_title = course_title.x)
+        
+        not_req = is.na(df$status)
+        df$status[not_req] = "elective"
+        no_title = is.na(df$course_title)
+        df$course_title[no_title] = df$course_title.y[no_title]
+        df$public_health = grepl("PH", df$course_number)
+        df$taken = !is.na(df$year)
+        grades = df$grade
+        df = df %>% 
+            group_by(student_id) %>% 
+            select(course_number, course_title, status,
+                   program, student_id, 
+                   taken, year, public_health)
+        df$grade = grades
+        df
+    }, options = list(pageLength = 10))
+    
+    
 }
 
 # Run the application 
